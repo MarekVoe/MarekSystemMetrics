69d68281b9470a1336b837e1cc42300c2d3c3666
MarekVoe <marekhrachovec3@gmail.com>
2025-05-22 16:35:24 +0200

    initialize MarekSystemMetrics project with Spring Boot and Maven setup

---
+++ src/main/java/dev/marekvoe/mareksystemmetrics/MarekSystemMetricsApplication.java
@@ -0,0 +1,13 @@
+package dev.marekvoe.mareksystemmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarekSystemMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarekSystemMetricsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/marekvoe/mareksystemmetrics/MarekSystemMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marekvoe.mareksystemmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarekSystemMetricsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
